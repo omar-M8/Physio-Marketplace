e3b87d496ca46f5a86d68ec57e8c249d338c7c96
Omar <mohammed.omar078@gmail.com>
2025-03-08 14:17:49 -0800

    Create home dashboard for the user

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Define the properties that the component will accept
+
+const Home: React.FC = () => {
+    return (
+        <main>
+            <h1>Physio Marketplace</h1>
+            <h2>Welcome</h2>
+            <br/>
+            {/* <button onClick={}>Logout</button> */}
+        </main>
+    )
+}
+
+export default Home;
