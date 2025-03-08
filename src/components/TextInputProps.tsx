d8bdde785fea21b3625b48e10586a622399f0191
Omar <mohammed.omar078@gmail.com>
2025-03-08 12:23:29 -0800

    Update component state to react to any changes in the input field

--- src/components/TextInputProps.tsx
+++ src/components/TextInputProps.tsx
@@ -1,18 +1,22 @@
 
 import React from 'react';
 
+// Define property types with TypeScript
 type TextInputProps = {
     label: string;
     type: string;
     name: string;
     required: boolean;
+    value: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextInput: React.FC<TextInputProps> = ({label, name, type, required = false}) => {
+// Define component that returns JSX
+const TextInput: React.FC<TextInputProps> = ({label, name, type, required = false, value, onChange}) => {
     return (
         <label>
             {label}
-            <input type={type} name={name} required={required} />
+            <input type={type} name={name} required={required} value={value} onChange={onChange}/>
         </label>
     )
 }
--- src/components/passwordInputProp.tsx
+++ src/components/passwordInputProp.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
+//Define the properties that the component will accept
 type PasswordInputProps = {
     label: string;
+    type: string;
     name: string;
     required?: true;
+    value: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const passwordInputs: React.FC<PasswordInputProps> = ({label, name, required = false}) => {
+//Define component that returns JSX
+const passwordInputs: React.FC<PasswordInputProps> = ({label, type, name, required = false, value, onChange}) => {
     return (
         <label>
             {label}
-            <input type='password' name={name} required={required} />
+            <input name={name} type={type} required={required} value={value} onChange={onChange} />
         </label>
     )
 }
