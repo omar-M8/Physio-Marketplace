0cd2ee931ce254e4e9273da5c9b325d330357d0b
Omar <mohammed.omar078@gmail.com>
2025-03-19 11:55:11 -0700

    Add logout button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 // Define the properties that the component will accept
 
 const Home: React.FC = () => {
+
+    const navigate = useNavigate();
+
+    const toLandingPage = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("Navigate to Landing page")
+        navigate('/')
+    }
+
     return (
         <main>
             <h1>Physio Marketplace</h1>
             <h2>Welcome</h2>
             <br/>
-            {/* <button onClick={}>Logout</button> */}
+            <button onClick={toLandingPage}>Logout</button>
         </main>
     )
 }
